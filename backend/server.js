24702a649789a1b9f8280532b0935aad855ed50a
Cyril Marois <cyrilmarois@gmail.com>
2022-09-09 22:25:48 +0200

    feat(backend): create server.js

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,14 @@
 
 const express = require("express");
 
-// CONST declaration
 const PORT = 666;
 const HOST = "0.0.0.0";
-
-// App
 const app = express();
+
 app.get("/", (req, res) => {
   res.send("Hello World !");
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
